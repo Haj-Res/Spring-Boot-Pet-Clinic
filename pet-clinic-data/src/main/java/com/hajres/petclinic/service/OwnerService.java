4b674bfac8916a1ea94b9546bf0f55470d02e319
Resad Hajdarpasic <r.hajdarpasic@dvcsolutions.com>
2020-05-29 14:27:42 +0200

    feat: impl. interfaces for services
    Closes #5

---
+++ pet-clinic-data/src/main/java/com/hajres/petclinic/service/OwnerService.java
@@ -0,0 +1,15 @@
+package com.hajres.petclinic.service;
+
+import com.hajres.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
