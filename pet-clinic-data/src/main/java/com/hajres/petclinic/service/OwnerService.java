c537be5708335ee69c6573c7d7834c83d7dfd47a
Resad Hajdarpasic <r.hajdarpasic@dvcsolutions.com>
2020-06-01 10:17:54 +0200

    refactor: refactor to common crud interface.
    Closes #16

--- pet-clinic-data/src/main/java/com/hajres/petclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/hajres/petclinic/service/OwnerService.java
@@ -2,14 +2,6 @@ package com.hajres.petclinic.service;
 
 import com.hajres.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
