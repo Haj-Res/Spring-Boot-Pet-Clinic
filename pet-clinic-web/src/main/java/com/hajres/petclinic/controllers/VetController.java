03f7a2c3112ccd840fba97d91f46ae88fd9f4ce8
Resad Hajdarpasic <r.hajdarpasic@dvcsolutions.com>
2020-06-01 15:12:39 +0200

    feat: add vet list to vet index
    Closes #23

--- pet-clinic-web/src/main/java/com/hajres/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/hajres/petclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package com.hajres.petclinic.controllers;
 
+import com.hajres.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/vets")
 public class VetController {
+    private final VetService vetService;
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
