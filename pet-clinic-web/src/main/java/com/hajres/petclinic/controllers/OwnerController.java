4ba7f542b1dc87f2352eccf7cd41f3aec70ad2ef
Resad Hajdarpasic <r.hajdarpasic@dvcsolutions.com>
2020-06-01 15:05:26 +0200

    feat: add owner list to owner index page
    Closes #24

--- pet-clinic-web/src/main/java/com/hajres/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/hajres/petclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package com.hajres.petclinic.controllers;
 
+import com.hajres.petclinic.model.Owner;
+import com.hajres.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
