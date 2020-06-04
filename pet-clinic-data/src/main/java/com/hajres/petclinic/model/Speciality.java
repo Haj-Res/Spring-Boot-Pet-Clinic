3e7d2b9f32c4796eda299c804c8a1429c5bec44c
Resad Hajdarpasic <r.hajdarpasic@dvcsolutions.com>
2020-06-04 10:20:24 +0200

    feat: convert POJO models to JPA entities
    Closes #60, Closes #61, Closes #62, Closes #63

--- pet-clinic-data/src/main/java/com/hajres/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/hajres/petclinic/model/Speciality.java
@@ -1,7 +1,20 @@
 package com.hajres.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
+
+    @ManyToMany(mappedBy = "specialities")
+    private Set<Vet> vets = new HashSet<>();
 
     public String getDescription() {
         return description;
@@ -9,5 +22,13 @@ public class Speciality extends BaseEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Set<Vet> getVets() {
+        return vets;
+    }
+
+    public void setVets(Set<Vet> vets) {
+        this.vets = vets;
     }
 }
